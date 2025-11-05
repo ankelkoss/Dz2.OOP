63058722217450a58be8896fd102d792e3f3b320
ankelkoss <70823492+ankelkoss@users.noreply.github.com>
2025-11-05 20:40:02 +0200

    add volin class

---
+++ MoneyAndGoods/Task2/Violin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace MoneyAndGoods.Task2
+{
+    public class Violin : MusicalInstrument
+    {
+        public Violin()
+        {
+            Name = "Скрипка";
+            Description = "Струнный смычковый музыкальный инструмент с 4 струнами. \r\nИмеет высокий регистр и выразительный тембр.";
+            Histry = "Скрипка появилась в XVI веке в Италии. \r\nСовременный вид приобрела в мастерских Амати, Страдивари и Гварнери.";
+        }
+
+        public override void Show() => Console.WriteLine($"Инструмент: {Name}");
+        public override void Desc() => Console.WriteLine($"Описание: {Description}");
+        public override void History() => Console.WriteLine($"История: {Histry}");
+    }
+}
