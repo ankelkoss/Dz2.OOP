2b467453be6346a711f4e4bc3b6fc0ce7f24c4be
ankelkoss <70823492+ankelkoss@users.noreply.github.com>
2025-11-05 20:34:36 +0200

    add enum
    add currency enam

---
+++ MoneyAndGoods/Task1/СurrencyEnum.cs
@@ -0,0 +1,20 @@
+﻿namespace MoneyAndGoods.Task1
+{
+    public enum CurrencyEnum
+    {
+        USD,
+        EUR,
+        UAH
+    }
+
+    public static class CurrencyEnumExtensions
+    {
+        public static string ToRu(this CurrencyEnum v) => v switch
+        {
+            CurrencyEnum.UAH => "Грн",
+            CurrencyEnum.USD => "Доллар США",
+            CurrencyEnum.EUR => "Евро",
+            _ => v.ToString()
+        };
+    }
+}
